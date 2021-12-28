38f56d468e8792e6e6af7bba45564237407a43a9
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2021-12-28 16:32:39 +0530

    add contact info with icons

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,6 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy({ "src/css/": "css/" });
+    eleventyConfig.addPassthroughCopy({
+        "src/css/": "css/",
+        "src/icons": "icons/",
+    });
 };
