e7bf657bd0e7c08e3b8ff7c6c17c505fb3be8011
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2021-12-29 11:05:36 +0530

    track outbound link clicks using google analytics

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,20 @@
 require('dotenv').config();
 
+const parser = require('node-html-parser');
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy({
         "src/css/": "css/",
         "src/icons": "icons/",
+    });
+    eleventyConfig.addTransform('track-link-clicks', (content, outputPath) => {
+        if (outputPath.endsWith(".html")) {
+            const root = parser.parse(content);
+            root.querySelectorAll('a').forEach(a => {
+                const href = a.getAttribute('href');
+                a.setAttribute('onclick', `getOutboundLink('${href}'); return false;`);
+            })
+            return root.toString();
+        }
     });
 };
